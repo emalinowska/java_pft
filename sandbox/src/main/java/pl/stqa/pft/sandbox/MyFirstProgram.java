ca37e9b47d87ebde00812aea080c31c7df70b372
emalinowska <elgruszczynska@gmail.com>
2019-01-30 14:10:41 +0100

    first test

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,24 +9,17 @@ public class MyFirstProgram {
         hello("user");
         hello("Eliza");
 
-        Square s = new Square();
-        s.l = 5;
-        System.out.println("square area with sides " + s.l + " = " + area(s) );
+        Square s = new Square(5);
+        System.out.println("square area with sides " + s.l + " = " + s.area() );
 
-        Rectangle r = new Rectangle();
-        r.a = 4;
-        r.b = 6;
+        Rectangle r = new Rectangle(4, 6);
 
-        System.out.println("rectangle area with sides " + r.a + " and " + r.b + " = " + area(r));
+        System.out.println("rectangle area with sides " + r.a + " and " + r.b + " = " + r.area() );
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello," + somebody + "!");
             }
-
-    public static double area(Square s) {
-        return s.l * s.l;
-    }
 
     public static double area(Rectangle r) {
         return r.a * r.b;
