ae55cc452889176ac532068908317c8b6c7da755
emalinowska <elgruszczynska@gmail.com>
2019-01-30 14:10:59 +0100

    Create PointTests

---
+++ sandbox/src/test/java/pl/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,24 @@
+package pl.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance() {
+
+    Point point1 = new Point (0, -1);
+    Point point2 = new Point (0,2);
+    Assert.assertEquals(Point.distance(point1, point2), 3.0);
+  }
+
+  @Test
+  public void testDistanceFailed() {
+
+    Point point1 = new Point (2,2);
+    Point point2 = new Point (4,7);
+    Assert.assertEquals(Point.distance(point1, point2), 4 );
+
+  }
+}
